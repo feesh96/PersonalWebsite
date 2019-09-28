1d3cce064d7b970a584e08dfb8ac4d4786523903
Matthew Fishman <matthewfishman@Matthews-MacBook-Pro.local>
2019-09-28 16:55:18 -0400

    Routing for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([  //Put all the component routing within the home page here
+      { path: '', component: AboutComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'career', component: CareerComponent },
+      { path: 'hobbies', component: HobbiesComponent },
+      { path: 'contact', component: ContactComponent },
+    ])
+  ],
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    CareerComponent,
+    HobbiesComponent,
+    ContactComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
