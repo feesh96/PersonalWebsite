d86ec0abf5b5dcdf51355515157e2af7f2dacffe
Matthew Fishman <matthewfishman@Matthews-MBP.lan1>
2019-12-24 20:03:24 -0500

    Added interfaces/services and fixed player UI

---
+++ src/app/services/cloud.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { of } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CloudService {
+  constructor() { }
+
+  files: any = [
+    {
+      name: 'Evanesce',
+      url: '../../assets/Beats/Evanesce.mp3'
+    },
+    {
+      name: 'Moonwalking',
+      url: '../../assets/Beats/Moonwalking.mp3'
+    },
+    {
+      name: 'Mr. Big Chef',
+      url: '../../assets/Beats/Mr Big Chef.mp3'
+    },
+    {
+      name: 'My World',
+      url: '../../assets/Beats/My World.mp3'
+    },
+    {
+      name: 'One and Only',
+      url: '../../assets/Beats/One and Only.mp3'
+    },
+    {
+      name: 'The Good Life',
+      url: '../../assets/Beats/The Good Life.mp3'
+    }
+  ];
+
+  getFiles() {
+    return of(this.files);
+  }
+}
